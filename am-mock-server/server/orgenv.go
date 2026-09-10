4ef58105882dcb22443ea6950898d7c7f624fb8b
Benoit Bordigoni <benoit.bordigoni@graviteesource.com>
2026-09-10 10:48:02 +0200

    chore: add linting, license headers, and release pipeline
    Lint setup (vet, staticcheck, revive, license headers) mirroring
    gravitee-kubernetes-operator. Per-module release-please with independent
    semver tags, goreleaser for am-mock-server binaries, and auto-bump
    workflow for common dependents.

--- am-mock-server/server/orgenv.go
+++ am-mock-server/server/orgenv.go
@@ -1,37 +1,51 @@
+// Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//         http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package server
 
 func (r ListDomainsRequestObject) GetOrgId() string  { return r.OrgId }
-func (r ListDomainsRequestObject) GetEnvId() string   { return r.EnvId }
-func (r UpsertDomainRequestObject) GetOrgId() string  { return r.OrgId }
-func (r UpsertDomainRequestObject) GetEnvId() string   { return r.EnvId }
-func (r DeleteDomainRequestObject) GetOrgId() string  { return r.OrgId }
-func (r DeleteDomainRequestObject) GetEnvId() string   { return r.EnvId }
-func (r GetDomainRequestObject) GetOrgId() string     { return r.OrgId }
-func (r GetDomainRequestObject) GetEnvId() string      { return r.EnvId }
+func (r ListDomainsRequestObject) GetEnvId() string  { return r.EnvId }
+func (r UpsertDomainRequestObject) GetOrgId() string { return r.OrgId }
+func (r UpsertDomainRequestObject) GetEnvId() string { return r.EnvId }
+func (r DeleteDomainRequestObject) GetOrgId() string { return r.OrgId }
+func (r DeleteDomainRequestObject) GetEnvId() string { return r.EnvId }
+func (r GetDomainRequestObject) GetOrgId() string    { return r.OrgId }
+func (r GetDomainRequestObject) GetEnvId() string    { return r.EnvId }
 
 func (r ListCertificatesRequestObject) GetOrgId() string  { return r.OrgId }
-func (r ListCertificatesRequestObject) GetEnvId() string   { return r.EnvId }
-func (r UpsertCertificateRequestObject) GetOrgId() string  { return r.OrgId }
-func (r UpsertCertificateRequestObject) GetEnvId() string   { return r.EnvId }
-func (r DeleteCertificateRequestObject) GetOrgId() string  { return r.OrgId }
-func (r DeleteCertificateRequestObject) GetEnvId() string   { return r.EnvId }
-func (r GetCertificateRequestObject) GetOrgId() string     { return r.OrgId }
-func (r GetCertificateRequestObject) GetEnvId() string      { return r.EnvId }
+func (r ListCertificatesRequestObject) GetEnvId() string  { return r.EnvId }
+func (r UpsertCertificateRequestObject) GetOrgId() string { return r.OrgId }
+func (r UpsertCertificateRequestObject) GetEnvId() string { return r.EnvId }
+func (r DeleteCertificateRequestObject) GetOrgId() string { return r.OrgId }
+func (r DeleteCertificateRequestObject) GetEnvId() string { return r.EnvId }
+func (r GetCertificateRequestObject) GetOrgId() string    { return r.OrgId }
+func (r GetCertificateRequestObject) GetEnvId() string    { return r.EnvId }
 
 func (r ListIdentityProvidersRequestObject) GetOrgId() string  { return r.OrgId }
-func (r ListIdentityProvidersRequestObject) GetEnvId() string   { return r.EnvId }
-func (r UpsertIdentityProviderRequestObject) GetOrgId() string  { return r.OrgId }
-func (r UpsertIdentityProviderRequestObject) GetEnvId() string   { return r.EnvId }
-func (r DeleteIdentityProviderRequestObject) GetOrgId() string  { return r.OrgId }
-func (r DeleteIdentityProviderRequestObject) GetEnvId() string   { return r.EnvId }
-func (r GetIdentityProviderRequestObject) GetOrgId() string     { return r.OrgId }
-func (r GetIdentityProviderRequestObject) GetEnvId() string      { return r.EnvId }
+func (r ListIdentityProvidersRequestObject) GetEnvId() string  { return r.EnvId }
+func (r UpsertIdentityProviderRequestObject) GetOrgId() string { return r.OrgId }
+func (r UpsertIdentityProviderRequestObject) GetEnvId() string { return r.EnvId }
+func (r DeleteIdentityProviderRequestObject) GetOrgId() string { return r.OrgId }
+func (r DeleteIdentityProviderRequestObject) GetEnvId() string { return r.EnvId }
+func (r GetIdentityProviderRequestObject) GetOrgId() string    { return r.OrgId }
+func (r GetIdentityProviderRequestObject) GetEnvId() string    { return r.EnvId }
 
 func (r ListReportersRequestObject) GetOrgId() string  { return r.OrgId }
-func (r ListReportersRequestObject) GetEnvId() string   { return r.EnvId }
-func (r UpsertReporterRequestObject) GetOrgId() string  { return r.OrgId }
-func (r UpsertReporterRequestObject) GetEnvId() string   { return r.EnvId }
-func (r DeleteReporterRequestObject) GetOrgId() string  { return r.OrgId }
-func (r DeleteReporterRequestObject) GetEnvId() string   { return r.EnvId }
-func (r GetReporterRequestObject) GetOrgId() string     { return r.OrgId }
-func (r GetReporterRequestObject) GetEnvId() string      { return r.EnvId }
+func (r ListReportersRequestObject) GetEnvId() string  { return r.EnvId }
+func (r UpsertReporterRequestObject) GetOrgId() string { return r.OrgId }
+func (r UpsertReporterRequestObject) GetEnvId() string { return r.EnvId }
+func (r DeleteReporterRequestObject) GetOrgId() string { return r.OrgId }
+func (r DeleteReporterRequestObject) GetEnvId() string { return r.EnvId }
+func (r GetReporterRequestObject) GetOrgId() string    { return r.OrgId }
+func (r GetReporterRequestObject) GetEnvId() string    { return r.EnvId }
--- common/pkg/apicontext/context.go
+++ common/pkg/apicontext/context.go
@@ -1,3 +1,17 @@
+// Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//         http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package apicontext
 
 import (
@@ -7,6 +21,8 @@ import (
 	"github.com/gravitee-io-labs/gravitee-automation-sdks/common/pkg/errors"
 	"github.com/oapi-codegen/oapi-codegen/v2/pkg/securityprovider"
 )
+
+const defaultOrgEnv = "DEFAULT"
 
 type APIContext struct {
 	BaseURL string
@@ -56,16 +72,16 @@ func (c APIContext) AuthInterceptor() (func(ctx context.Context, req *http.Reque
 	return nil, errors.NewClientError(errors.NoAuthProvided)
 }
 
-func (c APIContext) getOrgID() string {
+func (c APIContext) GetOrgIDOrDefault() string {
 	if c.OrgID != "" {
 		return c.OrgID
 	}
-	return "DEFAULT"
+	return defaultOrgEnv
 }
 
-func (c APIContext) getEnvID() string {
+func (c APIContext) GetEnvIDOrDefault() string {
 	if c.EnvID != "" {
 		return c.EnvID
 	}
-	return "DEFAULT"
+	return defaultOrgEnv
 }
